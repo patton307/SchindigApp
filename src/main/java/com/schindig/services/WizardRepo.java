3818cf2151bba77fa65144cffcc44d98248c7141
Agronis <agronis@icloud.com>
2015-12-09 16:14:56 -0500

    Wizard CSV & Method dump

--- src/main/java/com/schindig/services/WizardRepo.java
+++ src/main/java/com/schindig/services/WizardRepo.java
@@ -1,10 +1,16 @@
 package com.schindig.services;
 import com.schindig.entities.Wizard;
+import com.sun.tools.javac.util.List;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 /**
  * Created by Agronis on 12/9/15.
  */
 public interface WizardRepo extends CrudRepository<Wizard, Integer> {
+
+    @Query("SELECT COUNT(w) FROM Wizard w")
+    Integer wizardSize();
+
     
 }
