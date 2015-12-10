05d4ff8e16da04989273d3001dbed462639ae319
landonkail <landon.kail@gmail.com>
2015-12-10 17:24:30 -0500

    ....kk

--- src/main/java/com/schindig/controllers/MainController.java
+++ src/main/java/com/schindig/controllers/MainController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
@@ -13,7 +14,7 @@ import java.util.ArrayList;
 /**
  * Created by Agronis on 12/9/15.
  */
-@Controller
+@RestController
 public class MainController {
 
     @Autowired
