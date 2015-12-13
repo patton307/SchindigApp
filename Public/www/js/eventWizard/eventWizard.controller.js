e8469ce10fadf02f115b5bc3a5e947eb830fec89
Maxwell Krause <maxwell.l.krause@gmail.com>
2015-12-13 13:15:15 -0500

    added date stuff - merging before blakes build

--- Public/www/js/eventWizard/eventWizard.controller.js
+++ Public/www/js/eventWizard/eventWizard.controller.js
@@ -3,7 +3,7 @@
   angular
   .module("eventWizard")
 
-  .controller("EventWizardController", function($scope, $http, $stateParams, EventWizardService){
+  .controller("EventWizardController", function($scope, $http, $stateParams, $cordovaDatePicker, EventWizardService){
       var vm = this;
       EventWizardService.getWizard().then(function(data){
         $scope.wizardItems = data;
@@ -18,8 +18,28 @@
         };
         $scope.partySubType = $scope.get($stateParams);
       });
+      // DATE AND TIME PICKER
+    var options = {
+      date: new Date(),
+      mode: 'date', // or 'time'
+      minDate: new Date() - 10000,
+      allowOldDates: false,
+      allowFutureDates: true,
+      doneButtonLabel: 'DONE',
+      doneButtonColor: '#F2F3F4',
+      cancelButtonLabel: 'CANCEL',
+      cancelButtonColor: '#000000'
+    };
 
+
+    $scope.datePicker = function() {
+          $cordovaDatePicker.show(options).then(function(date){
+          alert(date);
   });
+};
+  //END DATE AND TIME PICKER
+});
+
 
 
 
