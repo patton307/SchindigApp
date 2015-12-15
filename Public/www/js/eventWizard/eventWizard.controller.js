1c66fe4fccb517eb16577eea56d57b7afde6a33d
Maxwell Krause <maxwell.l.krause@gmail.com>
2015-12-15 12:24:55 -0500

    trying to test things

--- Public/www/js/eventWizard/eventWizard.controller.js
+++ Public/www/js/eventWizard/eventWizard.controller.js
@@ -3,11 +3,8 @@
   angular
     .module("eventWizard")
 
-<<<<<<< HEAD
+
     .controller("EventWizardController", function($scope, $http, $state, $stateParams, $cordovaContacts, EventWizardService){
-=======
-    .controller("EventWizardController", function($scope, $http, $state, $stateParams, $cordovaDatePicker, EventWizardService){
->>>>>>> ba355c8bea43229dcdb3d27fbf4953700415e696
         var vm = this;
 
 
@@ -38,39 +35,7 @@
         });
       };
 
-<<<<<<< HEAD
-        ///POST DATE, TIME AND NAME/////
-       $scope.dateAndTimePost = function(partyDate, partyName){
-         var partyID = +localStorage.getItem('partyID');
-         console.log('partyId in localstorage', partyID);
-         var data = {
-           partyDate: partyDate,
-           partyName: partyName,
-           partyID: partyID
-         };
-         data.partyDate = JSON.stringify(data.partyDate);
-         data.partyDate = JSON.parse(data.partyDate);
-         console.log('updated party data: ', data);
-         EventWizardService.updateWizData(data).success(function(updatedWizData){
-           console.log('promise return of updated wizdata', updatedWizData);
-           $state.go('favors');
-         });
-       };
 
-
-     //STRETCHGOAL POST and SCOPES//
-     $scope.stretchGoalData = function (stretchStatus, stretchGoal, stretchName){
-       var partyID = +localStorage.getItem('partyID');
-       var data = {
-           stretchStatus,
-           stretchGoal,
-           stretchName,
-           partyID
-       };
-       console.log('updated stretchgoal:', data);
-       EventWizardService.updateWizData(data).success(function(updatedWizData){
-         console.log('new-stretchgoal', updatedWizData);
-=======
       ///POST DATE, TIME AND NAME/////
     $scope.dateAndTimePost = function(partyDate, partyName){
       var partyID = +localStorage.getItem('partyID');
@@ -105,7 +70,6 @@
        console.log('updated stretchgoal:', data);
        EventWizardService.updateWizData(data).success(function(updatedWizData){
          console.log('new-stretchgoal updated data', updatedWizData);
->>>>>>> ba355c8bea43229dcdb3d27fbf4953700415e696
          $state.go('invites');
        });
      };
