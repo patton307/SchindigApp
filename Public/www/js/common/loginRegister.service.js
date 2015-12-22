eb3ca0de4521e675ccea8d99f9103bae01be9de8
Maxwell Krause <maxwell.l.krause@gmail.com>
2015-12-22 10:40:27 -0500

    added test data

--- Public/www/js/common/loginRegister.service.js
+++ Public/www/js/common/loginRegister.service.js
@@ -3,14 +3,21 @@
 
   angular
     .module('loginRegister')
-    .factory('LoginRegisterService', function($http, $state, $q, $cordovaDevice){
 
-      var ip = 'http://10.0.10.72';
+    .factory('LoginRegisterService', function($http, $state, $q, $cordovaDevice){
+      var ip = 'http://localhost';
       var registerUrl = ip + ':8080/user/create';
       var loginUrl = ip + ':8080/user/login';
       // var uuidPostUrl = ip + ':8080/validate/' + uuid;
 
       // var uuid = $cordovaDevice.getUUID();
+
+      // var uuidAuth = function() {
+      //   return $http.get(uuidPostUrl).success(function(data){
+      //     console.log(data);
+      //
+      //   })
+      // }
 
       var login = function(loginData) {
         return $http.post(loginUrl, loginData)
