8f2ada964715c42e853001534f9d61974d6e6b7b
Maxwell Krause <maxwell.l.krause@gmail.com>
2015-12-28 21:39:29 -0500

    all done

--- Public/www/js/profile/profile.service.js
+++ Public/www/js/profile/profile.service.js
@@ -3,15 +3,20 @@
 
   angular
     .module('profile')
-    .factory('ProfileService', function($http){
+    .factory('ProfileService', function($http, $state){
+      var vm = this;
+      var ip = 'http://localhost:8080';
+      var userProfilePartiesURL = ip +'/user';
+      var userProfileUpdateURL = ip + '/user/update';
+
       var getData = function() {
-        console.log('profile service')
-        return $http.get('http://tiny-tiny.herokuapp.com/collections/ng-shoppingcart2')
-      }
+        console.log('profile service');
+        return $http.get('http://tiny-tiny.herokuapp.com/collections/ng-shoppingcart2');
+      };
       return {
         getData: getData
-      }
-    })
+      };
+    });
 
 
 }());
